c7ec026c570d7a2010c57c7d44965dfba230cbbf
roeialt <75533931+roeialt@users.noreply.github.com>
2020-12-05 16:56:15 +0200

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,12 @@
+RSCore = nil
+TriggerEvent('esx:getSharedObject', function(obj) RSCore = obj end)
+
+RegisterCommand("me", function(source , args , rawCommand)
+    args = table.concat(args, ' ')
+    TriggerClientEvent('me:event', -1, source, args, 0)
+end)
+
+RegisterCommand("do", function(source , args , rawCommand)
+    args = table.concat(args, ' ')
+    TriggerClientEvent('me:event', -1, source, args, 1)
+end)
